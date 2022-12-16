ac35286a5b71a1279e9d15d0d182e085faf81e88
JS2L <84076925+JS2L@users.noreply.github.com>
2022-12-16 13:28:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-3tck8b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ocdces)
---
+++ index.js
@@ -0,0 +1,6 @@
+// 다음 두 변수를 활용하여 삼각형의 너비를 출력해 보세요.
+let base = 10;
+let height = 5;
+
+// console.log()내부에 삼각형의 너비를 구하는 코드를 작성하세요.
+console.log((base * height) / 2);
